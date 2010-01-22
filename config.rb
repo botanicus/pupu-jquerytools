e7d6deecae8e61741273d26cff2b4c44c0fb18d5
Jakub Stastny aka Botanicus <knava.bestvinensis@gmail.com>
2010-01-22 15:29:27 +0000

    Initial import

---
+++ config.rb
@@ -0,0 +1,12 @@
+# dependencies of the pupu
+
+# load javascripts
+javascript "jquerytools"
+
+# load stylesheets
+
+# parameters of plugin
+# it will be blank in most cases
+#parameter :request, :optional => ["local", "ajax"] do |type|
+#  javascript "lib/autocompleter.#{type}"
+#end
